cf4b7fae2c09e7ca1c1057ca5dde351b0ef8d68a
Daniel Meza <daniel.meza29@yahoo.com>
2020-04-28 16:10:26 -0500

    Changed fetch to work with for loops

--- scripts/fetchingData.js
+++ scripts/fetchingData.js
@@ -2,40 +2,37 @@ const fetchParkEntry = (parksInput) => {
   fetch(`https://data.nashville.gov/resource/74d7-b74t.json?$q=${parksInput}&$limit=3`)
     .then(parkEntries => parkEntries.json())  // Parse as JSON
     .then(parks => {
-      let name1 = parks[0].park_name;
-      const home1 = parks[0].mapped_location.human_address;
-      const obj1 = JSON.parse(home1);
-      let address1 = obj1.address;
+      const parksArray = [];
+      for (let i = 0; i < parks.length; i++) {
+        let name = parks[i].park_name;
+        const home = parks[i].mapped_location.human_address;
+        const obj = JSON.parse(home);
+        let address = obj.address;
+        parksArray.push({ "name": name, "address": address })
+      }
 
-      let name2 = parks[1].park_name;
-      const home2 = parks[1].mapped_location.human_address;
-      const obj2 = JSON.parse(home2);
-      let address2 = obj2.address;
-
-      let name3 = parks[2].park_name;
-      const home3 = parks[2].mapped_location.human_address;
-      const obj3 = JSON.parse(home3);
-      let address3 = obj3.address;
-
-      return [{ name: name1, address: address1 }, { name: name2, address: address2 }, { name: name3, address: address3 }];
+      console.log(parksArray);
+      //call John's function that's posting to the DOM
     }
     )
 }
+
+fetchParkEntry('soccer');
 
 const fetchArtEntry = (artsInput) => {
   fetch(`https://data.nashville.gov/resource/eviu-nxp6.json?$q=${artsInput}&$limit=3`)
     .then(artEntries => artEntries.json())  // Parse as JSON
     .then(arts => {
-      let name1 = arts[0].artwork;
-      let address1 = arts[0].location;
+      const artsArray = [];
+      for (let i = 0; i < arts.length; i++) {
+        let name = arts[i].artwork;
+        let address = arts[i].location;
+        artsArray.push({ "name": name, "address": address })
+      }
 
-      let name2 = arts[1].artwork;
-      let address2 = arts[1].location;
-
-      let name3 = arts[2].artwork;
-      let address3 = arts[2].location;
-
-      return [{ name: name1, address: address1 }, { name: name2, address: address2 }, { name: name3, address: address3 }];
+      console.log(artsArray);
     }
     )
 }
+
+fetchArtEntry('museum')

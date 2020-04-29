2c04d128fe51ffa2f35114e5c95725f8fb12a719
felipetempus <felipemouradev@gmail.com>
2020-04-29 10:22:01 -0400

    i'm dumb

--- scripts/arts.js
+++ scripts/arts.js
@@ -1,6 +1,4 @@
-let artDB = "https://data.nashville.gov/resource/eviu-nxp6.json"
-
 document.getElementById("artButton").addEventListener("click", () => {
     let artName = document.getElementById("artInput").value;
-    // console.log(artName)
+    fetchArtEntry(artName);
 })
